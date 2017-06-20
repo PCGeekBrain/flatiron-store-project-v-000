05c29cf17bc298035fcf0e2c20f3619d784b687e
Mendel Hornbacher <me@mendel.tech>
2017-06-20 18:58:06 -0400

    Basic model structure

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ActiveRecord::Base
+    has_many :line_items
+    has_many :items, through: :line_items
 
+    belongs_to :user
+
+    enum status: [:pending, :submitted]
+
+    def total
+        self.line_items.inject(0){|sum, line_item| sum + line_item.item.price * line_item.quantity}
+    end
 end
--- db/migrate/20140805183007_create_carts.rb
+++ db/migrate/20140805183007_create_carts.rb
@@ -1,5 +1,8 @@
 class CreateCarts < ActiveRecord::Migration
   def change
-
+    create_table :carts do |t|
+      t.integer :status
+      t.belongs_to :user
+    end
   end
 end
