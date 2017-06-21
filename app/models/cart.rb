62fd958bde448dab5795484835170dca07d36790
= <=>
2017-06-20 21:30:46 -0400

    model tests pass and dependencies install

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,4 +9,17 @@ class Cart < ActiveRecord::Base
     def total
         self.line_items.inject(0){|sum, line_item| sum + line_item.item.price * line_item.quantity}
     end
+
+    def add_item(item_id)
+        item_id = item_id.id if item_id.is_a?(Item)
+        line_item = self.line_items.find_by(item_id: item_id)
+
+        if line_item
+            #We already have the item in the cart
+            line_item.update(quantity: line_item.quantity + 1)
+        else
+            line_item = self.line_items.new(item_id: item_id)
+        end
+        return line_item
+    end
 end
